28a2696b50488c6ec56834e46cec2d9d8bd88b34
teamaamer <s12042173@stu.najah.edu>
2023-12-22 22:50:16 +0300

    h

--- RaneenRaed0/RaneenRaed/RaneenRaed/src/test/java/org/example/AcceptanceTest/acceptanceTest.java
+++ RaneenRaed0/RaneenRaed/RaneenRaed/src/test/java/org/example/AcceptanceTest/acceptanceTest.java
@@ -3,7 +3,6 @@ package org.example.AcceptanceTest;
 
         import io.cucumber.junit.Cucumber;
         import io.cucumber.junit.CucumberOptions;
-        import io.cucumber.junit.CucumberOptions.SnippetType;
         import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
